92270e08db71a5b562d12fa477d8938567e31ba7
Lucas Araujo <Lucas Araujo>
2015-10-28 18:32:18 -0200

    Starting Filters

--- app/js/controllers/EventController..js
+++ app/js/controllers/EventController..js
@@ -4,11 +4,6 @@
 eventsApp.controller('EventController',
 	function EventController($scope) {
 		
-		$scope.show = true;
-		$scope.buttonDisabled = true;
-		$scope.snippet = '<span style="color:red;">Hi, there</span>'
-		$scope.nonBindableSpace = true;
-		
 		$scope.event = {
 			name: "Angular Boot Camp",
 			date: "1/1/2013",
